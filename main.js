5f89af11998bfc0c513ca5933dd8183fa3ac1d13
linareyes <42820402+linareyes@users.noreply.github.com>
2018-11-23 18:47:19 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-//console.log("yeah");
-//window.scrollBy(100, 100);
-//alert(window.pageXOffset + window.pageYOffset);
 
-var counter = document.getElementById('counter');
 
-window.onscroll = function(){
-                  var distance = window.pageYOffset;
-                  console.log(distance);
-                  distance / 450;
-                  document.getElementById('counter').innerHTML = Math.floor(distance / 450)+ "K";
-
-                  if(distance > 2000){
-                    counter.style.backgroundcolor ="red";
-                  } else {
-                    document.getElementById('counter').style.backgroundcolor ="navy";
-                  }
-                  }
+var walk = document.getElementById("walk");
+function runAnimation(){
+walk.classList.add("restart");
+}
+function stopAnimation(){
+walk.classList.remove("restart");
+}
+// window.addEventListener('scroll', runAnimation);
+window.addEventListener('scroll', runAnimation);
+walk.addEventListener('animationend', stopAnimation);
