eaf1484a08faa87472ba70c11cafa0c7baa8f80b
linareyes <42820402+linareyes@users.noreply.github.com>
2018-11-29 14:31:54 -0800

    update

--- main.js
+++ main.js
@@ -12,14 +12,7 @@ walk.classList.remove("restart");
 window.addEventListener('scroll', runAnimation);
 walk.addEventListener('animationend', stopAnimation);
 
-
 function toggle(){
   document.getElementById('clip').classList.toggle('active')
 }
-
-$(document).on("ready", function() {
-    setTimeout(function() {
-        $(".letter").addClass("loaded");
-        $(".reg-text").addClass("loaded");
-    }, 1000);
-});
+ 
