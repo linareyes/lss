6a76befbecc6c006fc7cc6000643ebac407bdd74
linareyes <42820402+linareyes@users.noreply.github.com>
2018-11-28 22:02:29 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 
-
 var walk = document.getElementById("walk");
 function runAnimation(){
 walk.classList.add("restart");
 }
+
 function stopAnimation(){
 walk.classList.remove("restart");
 }
+
 // window.addEventListener('scroll', runAnimation);
 window.addEventListener('scroll', runAnimation);
 walk.addEventListener('animationend', stopAnimation);
+
+
+
+
+
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
